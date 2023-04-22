ee1b14d81b243434ab2f834218836492ebd4cdf2
PodmazowS <podmazowstas@gmail.com>
2023-04-22 14:41:04 +0200

    Add project files.

---
+++ An application to collect and share photos/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace An_application_to_collect_and_share_photos.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
