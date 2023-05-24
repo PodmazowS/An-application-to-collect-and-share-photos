c404e4dba8ccd8d23c2575ce947ddc19fdc39108
PodmazowS <podmazowstas@gmail.com>
2023-05-24 20:05:14 +0200

    Revert "update 2.4(add Photo, User, UserRole models and IPhoto, IUser, IUserRole repositories)"
    This reverts commit b3414d2339b5ac13899101bc90f859b4d734c7d9.

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -1,5 +1,4 @@
-﻿using MongoDB.Bson;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Models
 {
-    public class User
+    internal class User
     {
-        public ObjectId Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
-
+    }
 }
