149db504de993dffdb5dabea5c8c478363cd4b04
PodmazowS <podmazowstas@gmail.com>
2023-05-25 19:22:36 +0200

    update 2.7

---
+++ Infrastructure/Data/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Models;
+using Domain.Repositories;
+using MongoDB.Bson;
+using System.Numerics;
+
+namespace Infrastructure.Data
+{
+    public class UserRepository : IUserRepository
+
+    {
+        public void CreateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteUser(ObjectId userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(ObjectId userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
