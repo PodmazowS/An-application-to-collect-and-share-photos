cb90b50c17ad041c31a000cbf8099d7ff94339f8
Ivan Nedilko <untrusd@gmail.com>
2023-05-28 18:25:04 +0200

    Update 2.8 -Added MongoDBContext

---
+++ Infrastructure/MongoDB/MongoDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Models;
+using MongoDB.Bson;
+using MongoDB;
+
+public class MongoDBContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDBContext(string connectionString, string databaseName)
+    {
+        var client = new MongoClient(connectionString);
+        _database = client.GetDatabase(databaseName);
+    }
+
+    // Додайте власні властивості, які представляють колекції бази даних
+
+    public IMongoCollection<User> Users => _database.GetCollection<User>("users");
+    public IMongoCollection<Photo> Photos => _database.GetCollection<Photo>("photos");
+    // Додайте інші колекції, які використовуєте в проекті
+
+    // Додайте будь-які інші методи або функціональність, яку потребуєте
+
+}
