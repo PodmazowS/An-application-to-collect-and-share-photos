71417e2f7659674bbb2010f5389c6fc712f76c71
Ivan Nedilko <untrusd@gmail.com>
2023-05-29 20:07:00 +0200

    update 2.12 - Added AlbumRepository, PhotoRepository

---
+++ Domain/Services/AlbumService.cs
@@ -0,0 +1,106 @@
+﻿using Domain.Models;
+using Domain.Repositories;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class AlbumService
+    {
+        private readonly IAlbumRepository _albumRepository;
+
+        public AlbumService(IAlbumRepository albumRepository)
+        {
+            _albumRepository = albumRepository;
+        }
+
+        public async Task<IEnumerable<Album>> GetAllAlbums()
+        {
+            return await _albumRepository.GetAll();
+        }
+
+        public async Task<Album> GetAlbumByIdAsync(ObjectId id)
+        {
+            try
+            {
+                var album = await _albumRepository.GetById(id);
+
+                if (album == null)
+                {
+                    throw new Exception($"Album with id {id} not found.");
+                }
+
+                return album;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while retrieving photo with id {id}: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task CreateAlbumAsync(Album album)
+        {
+            try
+            {
+                var existingAlbum = await _albumRepository.GetById(album.Id);
+                    
+                if (existingAlbum != null)
+                {
+                    throw new Exception($"Album with id {album.Id} already exists.");
+                }
+
+                await _albumRepository.Create(album);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while creating the album: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task UpdateAlbumAsync(ObjectId id, Album album)
+        {
+            try
+            {
+                var existingAlbum = await _albumRepository.GetById(album.Id);
+
+                if (existingAlbum == null)
+                {
+                    throw new Exception($"Album with id {album.Id} doesn't exists.");
+                }
+
+                await _albumRepository.Update(id,album);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while updating the album: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task DeleteAlbumAsync(ObjectId id)
+        {
+            try
+            {
+                var existingAlbum = await _albumRepository.GetById(id);
+
+                if (existingAlbum == null)
+                {
+                    throw new Exception($"Album with id {id} doesn't exists.");
+                }
+
+                await _albumRepository.Delete(id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while deleting the album: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ Domain/Services/PhotoService.cs
@@ -0,0 +1,122 @@
+﻿using Domain.Models;
+using Domain.Repositories;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class PhotoService
+    {
+        private readonly IPhotoRepository _photoRepository;
+
+        public PhotoService(IPhotoRepository photoRepository)
+        {
+            _photoRepository = photoRepository;
+        }
+
+        public async Task<Photo> GetPhotoByIdAsync(ObjectId photoId)
+        {
+            try
+            {
+                var photo = await _photoRepository.GetPhotoByIdAsync(photoId);
+
+                if (photo == null)
+                {
+                    throw new Exception($"Photo with id {photoId} not found.");
+                }
+
+                return photo;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while retrieving photo with id {photoId}: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<Photo>> GetPhotosByUserIdAsync(ObjectId userId)
+        {
+            try
+            {
+                var photo = await _photoRepository.GetPhotosByUserIdAsync(userId);
+
+                if (photo == null)
+                {
+                    throw new Exception($"Photo with user id {userId} not found.");
+                }
+
+                return photo;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while retrieving photo with user id {userId}: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task CreatePhotoAsync(Photo photo)
+        {
+            try
+            {
+                var existingPhoto = await _photoRepository.GetPhotoByIdAsync(photo.Id);
+
+                if (existingPhoto != null)
+                {
+                    throw new Exception($"Photo with id {photo.Id} already exists.");
+                }
+
+                await _photoRepository.CreatePhotoAsync(photo);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"An error occurred while creating the photo: {ex.Message}");
+                throw; 
+            }
+        }
+
+        public async Task UpdatePhotoAsync(Photo photo)
+        {
+            try
+            {
+                var existingPhoto = await _photoRepository.GetPhotoByIdAsync(photo.Id);
+
+                if (existingPhoto == null)
+                {
+                    throw new Exception($"Photo with id {photo.Id} doesn't exist.");
+                }
+
+                await _photoRepository.UpdatePhotoAsync(photo);
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while updating the photo: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task DeletePhotoAsync(ObjectId photoId)
+        {
+            try
+            {
+                var existingPhoto = await _photoRepository.GetPhotoByIdAsync(photoId);
+
+                if (existingPhoto == null)
+                {
+                    throw new Exception($"Photo with id {photoId} doesn't exist.");
+                }
+
+                await _photoRepository.DeletePhotoAsync(photoId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while updating the photo: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
