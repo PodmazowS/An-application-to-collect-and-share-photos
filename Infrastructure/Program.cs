af506a65920d4efeba7f44aa02653896b8204dbe
Ivan Nedilko <untrusd@gmail.com>
2023-05-29 18:08:33 +0200

    Update 2.10

--- Infrastructure/Program.cs
+++ Infrastructure/Program.cs
@@ -1,3 +1,9 @@
+using Domain.Models;
+using Infrastructure.Data;
+using Infrastructure.MongoDB;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Infrastructure
 {
@@ -5,6 +11,7 @@ namespace Infrastructure
     {
         public static void Main(string[] args)
         {
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
@@ -14,6 +21,9 @@ namespace Infrastructure
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
+            builder.Services.AddSingleton<MongoDBContext>();
+ 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -24,11 +34,28 @@ namespace Infrastructure
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
-           
-            
+
+            var dbContext = app.Services.GetRequiredService<MongoDBContext>();
+
+            var photo = new Photo
+            {
+                Url = "https://example.com/photo.jpg",
+                Title = "New Photo",
+                Description = "This is a new photo",
+                AlbumId = ObjectId.GenerateNewId(),
+                UserId = ObjectId.GenerateNewId(),
+                CameraName = "Canon",
+                Status = "Active",
+                UploadDate = DateTime.Now,
+                Tag = "Nature"
+            };
+
+            dbContext.Photos.InsertOne(photo);
+
+            Console.WriteLine("Photo added to collection.");
+
 
             app.Run();
         }
