eeac2a72b16fe86c0f6e55a692dda9f92bc982d9
Ivan Nedilko <untrusd@gmail.com>
2023-05-31 21:35:11 +0200

    -update 3.1 updated Like, Comment Repositores

--- Infrastructure/Data/LikeRepository.cs
+++ Infrastructure/Data/LikeRepository.cs
@@ -1,39 +1,56 @@
 ﻿using Domain.Models;
 using Domain.Repositories;
+using Microsoft.Extensions.Hosting;
 using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Infrastructure.Data
 {
     public class LikeRepository : ILikeRepository
     {
+        private readonly IMongoCollection<Like> _likeCollection;
+
+        public LikeRepository(IMongoDatabase database)
+        {
+            _likeCollection = database.GetCollection<Like>("likes");
+        }
         public int AddLikeToPost(Like like, ObjectId id)
         {
-            throw new NotImplementedException();
+            like.PhotoId = id;
+            _likeCollection.InsertOne(like);
+            return 1; // Zwracamy 1 żeby pokazać że reakcja została pomyślnie dodana
         }
 
         public int DeleteLikeFromPost(Like like, ObjectId id)
         {
-            throw new NotImplementedException();
+            var filter = Builders<Like>.Filter.Eq(x => x.UserId, like.UserId) & Builders<Like>.Filter.Eq(x => x.PhotoId, id);
+            var result = _likeCollection.DeleteOne(filter);
+            return (int)result.DeletedCount;
         }
 
-        public Task<bool> HasUserLikedPhotoAsync(ObjectId userId, ObjectId photoId)
+        public async Task<bool> HasUserLikedPhotoAsync(ObjectId userId, ObjectId photoId)
         {
-            throw new NotImplementedException();
+            var filter = Builders<Like>.Filter.Eq(x => x.UserId, userId) & Builders<Like>.Filter.Eq(x => x.PhotoId, photoId);
+            var count = await _likeCollection.CountDocumentsAsync(filter);
+            return count > 0;
         }
 
-        public Task CreateLikeAsync(Like like)
+        public async Task CreateLikeAsync(Like like)
         {
-            throw new NotImplementedException();
+            await _likeCollection.InsertOneAsync(like);
         }
 
-        public Task DeleteLikeAsync(ObjectId userId, ObjectId photoId)
+        public async Task DeleteLikeAsync(ObjectId userId, ObjectId photoId)
         {
-            throw new NotImplementedException();
+            var filter = Builders<Like>.Filter.Eq(x => x.UserId, userId) & Builders<Like>.Filter.Eq(x => x.PhotoId, photoId);
+            await _likeCollection.DeleteOneAsync(filter);
         }
 
-        public Task<int> GetLikeCountForPhotoAsync(ObjectId photoId)
+        public async Task<int> GetLikeCountForPhotoAsync(ObjectId photoId)
         {
-            throw new NotImplementedException();
+            var filter = Builders<Like>.Filter.Eq(x => x.PhotoId, photoId);
+            var count = await _likeCollection.CountDocumentsAsync(filter);
+            return (int)count;
         }
     }
 }
