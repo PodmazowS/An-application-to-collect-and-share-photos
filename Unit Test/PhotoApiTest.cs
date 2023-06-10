dda866a2da74a3339cc8ab1831c5a883c76ff363
Руслан <@DESKTOP-R7J92JF>
2023-06-10 17:56:38 +0200

    test2

--- Unit Test/PhotoApiTest.cs
+++ Unit Test/PhotoApiTest.cs
@@ -9,11 +9,19 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Moq;
+using Domain.Models;
+using Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Infrastructure.MongoDB;
 
 namespace Unit_Test
 {
     public class PhotoApiTest : IClassFixture<WebApplicationFactory<Program>>
     {
+
+
+
         //Arrange for all methods 
         private readonly HttpClient _client;
 
@@ -22,7 +30,7 @@ namespace Unit_Test
         {
             _client = factory.CreateClient();
         }
-        
+
 
 
         [Fact]//it's correct        
@@ -41,10 +49,11 @@ namespace Unit_Test
 
 
 
+
         [Fact]//it's correct
         public async void GetShouldReturnOkStatus()
         {
-            
+
             //await using var application = new WebApplicationFactory<Program>();
             //using var client = application.CreateClient();
 
@@ -57,54 +66,101 @@ namespace Unit_Test
         }
 
 
-        
 
-        
-        [Fact]
-        public async Task GetPhotoByIdAsync_WithValidId_ReturnsPhoto()
-        {
-            // Arrange
-            var validId = 1;
-                    
-            // Act
-            var result = await _client.GetAsync($"/api/PhotoControllerMongoDb/{validId}");
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
-
-            var content = await result.Content.ReadAsStringAsync();
-            var photo = JsonConvert.DeserializeObject<PhotoDto>(content);
-
-            Assert.NotNull(photo);
-            Assert.Equal(validId.ToString(), photo.Id);
-        }
-        
-
-
-
-
-
-        //?
-        /*
-        [Fact]
-        public async void PostTest()
-        {
-
-            HttpRequestMessage request = new HttpRequestMessage()
+            /*
+            [Fact]
+            public async Task GetPhotoByIdAsync_WithValidId_ReturnsPhotoDto()
             {
-                RequestUri = new Uri("https://localhost:7077/api/books"),
-                Method = HttpMethod.Post,
-                Headers =
-                    {
-                        {HttpRequestHeader.Authorization.ToString(), "Bearer 3789..."},
-                        {HttpRequestHeader.ContentType.ToString(), "application/json"}
-                    },
-                Content = JsonContent.Create(body),
-            };
+                // Arrange
+                var validId = ObjectId.GenerateNewId();
+                var photo = new Photo
+                {
+                    Id = validId,// 
+                    Url = "https://example.com/photo.jpg",
+                    Title = "Sample Photo",
+                    Description = "This is a sample photo",
+                    CameraName = "Nikon",
+                    Status = "Active",
+                    UploadDate = DateTime.UtcNow,
+                    Tag = "Sample",
+                    UserId = ObjectId.GenerateNewId()//
+                };
 
-            var response = await _client.SendAsync(request);
-           
-        }
-        */
+                var mockService = new Mock<PhotoServiceMongoDb>();
+                mockService.Setup(s => s.GetPhotoByIdAsync(validId)).ReturnsAsync(photo);
+
+                var controller = new PhotoControllerMongoDb(mockService.Object);//PhotoController
+
+                // Act
+                var result = await controller.FindById(validId);
+
+                // Assert
+                var okResult = Assert.IsType<OkObjectResult>(result);
+                var photoDto = Assert.IsType<PhotoDto>(okResult.Value);
+
+                Assert.Equal(photo.Id, photoDto.Id);
+                Assert.Equal(photo.Url, photoDto.Url);
+                Assert.Equal(photo.Title, photoDto.Title);
+                Assert.Equal(photo.Description, photoDto.Description);
+                Assert.Equal(photo.CameraName, photoDto.CameraName);
+                Assert.Equal(photo.Status, photoDto.Status);
+                Assert.Equal(photo.UploadDate, photoDto.UploadDate);
+                Assert.Equal(photo.Tag, photoDto.Tag);
+                Assert.Equal(photo.UserId, photoDto.UserId);
+            }
+            */
+
+
+
+            //dont work
+            /*
+            [Fact]
+            public async Task GetPhotoByIdAsync_WithValidId_ReturnsPhoto()
+            {
+                // Arrange
+                var validId = 1;
+
+                // Act
+                var result = await _client.GetAsync($"/api/PhotoControllerMongoDb/{validId}");
+
+                // Assert
+                Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+
+                var content = await result.Content.ReadAsStringAsync();
+                var photo = JsonConvert.DeserializeObject<PhotoDto>(content);
+
+                Assert.NotNull(photo);
+                Assert.Equal(validId.ToString(), photo.Id);
+            }
+            */
+
+
+
+
+
+
+            //?
+            /*
+            [Fact]
+            public async void PostTest()
+            {
+
+                HttpRequestMessage request = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri("https://localhost:7077/api/books"),
+                    Method = HttpMethod.Post,
+                    Headers =
+                        {
+                            {HttpRequestHeader.Authorization.ToString(), "Bearer 3789..."},
+                            {HttpRequestHeader.ContentType.ToString(), "application/json"}
+                        },
+                    Content = JsonContent.Create(body),
+                };
+
+                var response = await _client.SendAsync(request);
+
+            }
+            */
+        
     }
 }
